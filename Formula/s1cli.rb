e86650f44e84d03cfc8f0ce161e7d7f713210736
Geoion <Eski.Yin@Gmail.com>
2026-03-08 03:29:44 +0800

    fix(s1cli): use full PyPI URLs with hash paths for all resources
    Replace short PyPI URLs with full content-addressed URLs to fix
    Homebrew download failures. All resource URLs now include the
    correct hash-based directory path required by PyPI CDN.

--- Formula/s1cli.rb
+++ Formula/s1cli.rb
@@ -10,107 +10,107 @@ class S1cli < Formula
   depends_on "python@3.12"
 
   resource "anyio" do
-    url "https://files.pythonhosted.org/packages/anyio-4.12.1-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/38/0e/27be9fdef66e72d64c0cdc3cc2823101b80585f8119b5c112c2e8f5f7dab/anyio-4.12.1-py3-none-any.whl"
     sha256 "d405828884fc140aa80a3c667b8beed277f1dfedec42ba031bd6ac3db606ab6c"
   end
 
   resource "beautifulsoup4" do
-    url "https://files.pythonhosted.org/packages/beautifulsoup4-4.14.3-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/1a/39/47f9197bdd44df24d67ac8893641e16f386c984a0619ef2ee4c51fbbc019/beautifulsoup4-4.14.3-py3-none-any.whl"
     sha256 "0918bfe44902e6ad8d57732ba310582e98da931428d231a5ecb9e7c703a735bb"
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/certifi-2026.2.25-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/9a/3c/c17fb3ca2d9c3acff52e30b309f538586f9f5b9c9cf454f3845fc9af4881/certifi-2026.2.25-py3-none-any.whl"
     sha256 "027692e4402ad994f1c42e52a4997a9763c646b73e4096e4d5d6db8af1d6f0fa"
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/click-8.3.1-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/98/78/01c019cdb5d6498122777c1a43056ebb3ebfeef2076d9d026bfe15583b2b/click-8.3.1-py3-none-any.whl"
     sha256 "981153a64e25f12d547d3426c367a4857371575ee7ad18df2a6183ab0545b2a6"
   end
 
   resource "h11" do
-    url "https://files.pythonhosted.org/packages/h11-0.16.0-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/04/4b/29cac41a4d98d144bf5f6d33995617b185d14b22401f75ca86f384e87ff1/h11-0.16.0-py3-none-any.whl"
     sha256 "63cf8bbe7522de3bf65932fda1d9c2772064ffb3dae62d55932da54b31cb6c86"
   end
 
   resource "httpcore" do
-    url "https://files.pythonhosted.org/packages/httpcore-1.0.9-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/7e/f5/f66802a942d491edb555dd61e3a9961140fd64c90bce1eafd741609d334d/httpcore-1.0.9-py3-none-any.whl"
     sha256 "2d400746a40668fc9dec9810239072b40b4484b640a8c38fd654a024c7a1bf55"
   end
 
   resource "httpx" do
-    url "https://files.pythonhosted.org/packages/httpx-0.28.1-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/2a/39/e50c7c3a983047577ee07d2a9e53faf5a69493943ec3f6a384bdc792deb2/httpx-0.28.1-py3-none-any.whl"
     sha256 "d909fcccc110f8c7faf814ca82a9a4d816bc5a6dbfea25d6591d6985b8ba59ad"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/idna-3.11-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/0e/61/66938bbb5fc52dbdf84594873d5b51fb1f7c7794e9c0f5bd885f30bc507b/idna-3.11-py3-none-any.whl"
     sha256 "771a87f49d9defaf64091e6e6fe9c18d4833f140bd19464795bc32d966ca37ea"
   end
 
   resource "linkify-it-py" do
-    url "https://files.pythonhosted.org/packages/linkify_it_py-2.1.0-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/b4/de/88b3be5c31b22333b3ca2f6ff1de4e863d8fe45aaea7485f591970ec1d3e/linkify_it_py-2.1.0-py3-none-any.whl"
     sha256 "0d252c1594ecba2ecedc444053db5d3a9b7ec1b0dd929c8f1d74dce89f86c05e"
   end
 
   resource "lxml" do
-    url "https://files.pythonhosted.org/packages/lxml-6.0.2.tar.gz"
+    url "https://files.pythonhosted.org/packages/aa/88/262177de60548e5a2bfc46ad28232c9e9cbde697bd94132aeb80364675cb/lxml-6.0.2.tar.gz"
     sha256 "cd79f3367bd74b317dda655dc8fcfa304d9eb6e4fb06b7168c5cf27f96e0cd62"
   end
 
   resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/markdown_it_py-4.0.0-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/94/54/e7d793b573f298e1c9013b8c4dade17d481164aa517d1d7148619c2cedbf/markdown_it_py-4.0.0-py3-none-any.whl"
     sha256 "87327c59b172c5011896038353a81343b6754500a08cd7a4973bb48c6d578147"
   end
 
   resource "mdit-py-plugins" do
-    url "https://files.pythonhosted.org/packages/mdit_py_plugins-0.5.0-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/fb/86/dd6e5db36df29e76c7a7699123569a4a18c1623ce68d826ed96c62643cae/mdit_py_plugins-0.5.0-py3-none-any.whl"
     sha256 "07a08422fc1936a5d26d146759e9155ea466e842f5ab2f7d2266dd084c8dab1f"
   end
 
   resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/mdurl-0.1.2-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/b3/38/89ba8ad64ae25be8de66a6d463314cf1eb366222074cfda9ee839c56a4b4/mdurl-0.1.2-py3-none-any.whl"
     sha256 "84008a41e51615a49fc9966191ff91509e3c40b939176e643fd50a5c2196b8f8"
   end
 
   resource "platformdirs" do
-    url "https://files.pythonhosted.org/packages/platformdirs-4.9.4-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/63/d7/97f7e3a6abb67d8080dd406fd4df842c2be0efaf712d1c899c32a075027c/platformdirs-4.9.4-py3-none-any.whl"
     sha256 "68a9a4619a666ea6439f2ff250c12a853cd1cbd5158d258bd824a7df6be2f868"
   end
 
   resource "pygments" do
-    url "https://files.pythonhosted.org/packages/pygments-2.19.2-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/c7/21/705964c7812476f378728bdf590ca4b771ec72385c533964653c68e86bdc/pygments-2.19.2-py3-none-any.whl"
     sha256 "86540386c03d588bb81d44bc3928634ff26449851e99741617ecb9037ee5ec0b"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/rich-14.3.3-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/14/25/b208c5683343959b670dc001595f2f3737e051da617f66c31f7c4fa93abc/rich-14.3.3-py3-none-any.whl"
     sha256 "793431c1f8619afa7d3b52b2cdec859562b950ea0d4b6b505397612db8d5362d"
   end
 
   resource "soupsieve" do
-    url "https://files.pythonhosted.org/packages/soupsieve-2.8.3-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/46/2c/1462b1d0a634697ae9e55b3cecdcb64788e8b7d63f54d923fcd0bb140aed/soupsieve-2.8.3-py3-none-any.whl"
     sha256 "ed64f2ba4eebeab06cc4962affce381647455978ffc1e36bb79a545b91f45a95"
   end
 
   resource "textual" do
-    url "https://files.pythonhosted.org/packages/textual-8.0.2-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/77/bc/0cd17f96f00b6e8bfbca64c574088c85f3c614912b3030f313752e30a099/textual-8.0.2-py3-none-any.whl"
     sha256 "4ceadbe0e8a30eb80f9995000f4d031f711420a31b02da38f3482957b7c50ce4"
   end
 
   resource "toml" do
-    url "https://files.pythonhosted.org/packages/toml-0.10.2-py2.py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/44/6f/7120676b6d73228c96e17f1f794d8ab046fc910d781c8d151120c3f1569e/toml-0.10.2-py2.py3-none-any.whl"
     sha256 "806143ae5bfb6a3c6e736a764057db0e6a0e05e338b5630894a5f779cabb4f9b"
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/typing_extensions-4.15.0-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/18/67/36e9267722cc04a6b9f15c7f3441c2363321a3ea07da7ae0c0707beb2a9c/typing_extensions-4.15.0-py3-none-any.whl"
     sha256 "f0fa19c6845758ab08074a0cfa8b7aecb71c999ca73d62883bc25cc018c4e548"
   end
 
   resource "uc-micro-py" do
-    url "https://files.pythonhosted.org/packages/uc_micro_py-2.0.0-py3-none-any.whl"
+    url "https://files.pythonhosted.org/packages/61/73/d21edf5b204d1467e06500080a50f79d49ef2b997c79123a536d4a17d97c/uc_micro_py-2.0.0-py3-none-any.whl"
     sha256 "3603a3859af53e5a39bc7677713c78ea6589ff188d70f4fee165db88e22b242c"
   end
 
